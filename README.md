d752eedf34ae1aa99c77eeff8fc796194ccef3b4
Jack <jack20220723@gmail.com>
2024-11-06 21:02:45 +0800

    Load German vocabulary

---
+++ README.md
@@ -0,0 +1,66 @@
+Wilhelm Graph Database
+======================
+
+[![Neo4J DB version badge]][Neo4J Docker version]
+[![Apache License Badge]][Apache License, Version 2.0]
+
+__wilhelm-graphdb__ is a Docker image that hosts Wiktionary languge data in Neo4J graph database. It is part of the
+efforts that
+[scales project Wilhelm](https://github.com/QubitPi/wilhelm?tab=readme-ov-file#why-do-i-decide-to-scale-project-wilhelm)
+
+> [!NOTE]
+> 
+> wilhelm-graphdb is under active development and will start serving alone with
+> [paion-data/aristotle](https://aristotle-ws.com/)
+
+Development
+-----------
+
+#### Updating the Datasource
+
+We use Wiktionary (English) as the vocabulary source. Thanks to the awesome works by
+[tatuylonen](https://github.com/tatuylonen/wiktextract), the source data is already
+[in JSON format](https://kaikki.org/dictionary/rawdata.html). The source data is going to be updated regularly so to get
+the latest data, simply download the __raw Wiktextract data (JSONL, one object per line)__
+
+#### Start a Local Neo4J Database
+
+```console
+docker run \
+    --publish=7474:7474 \
+    --publish=7687:7687 \
+    --env=NEO4J_AUTH=none \
+    --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes neo4j:5.24-enterprise
+```
+
+To manually 
+
+```console
+python3 -m virtualenv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+```console
+pip3 uninstall wilhelm_python_sdk
+pip3 install --upgrade --force-reinstall wilhelm-python-sdk
+```
+
+```console
+python3 load_german.py
+```
+
+> [!WARNING]
+> 
+> The loading takes a long time, please be patient
+
+License
+-------
+
+The use and distribution terms for [wilhelm-vocabulary]() are covered by the [Apache License, Version 2.0].
+
+[Apache License Badge]: https://img.shields.io/badge/Apache%202.0-F25910.svg?style=for-the-badge&logo=Apache&logoColor=white
+[Apache License, Version 2.0]: https://www.apache.org/licenses/LICENSE-2.0
+
+[Neo4J DB version badge]: https://img.shields.io/badge/Neo4J-5.24--enterprise-4581C3.svg?style=for-the-badge&logo=neo4j&logoColor=white
+[Neo4J Docker version]: https://hub.docker.com/_/neo4j/tags?name=5.24-enterprise
---
+++ load_german.py
@@ -0,0 +1,58 @@
+# Copyright Jiaqi Liu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import json
+import os
+from wilhelm_python_sdk.database_clients import get_database_client
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Generate JSON data')
+    parser.add_argument('-i', '--input', help='Raw data file, i.e. the path to raw-wiktextract-data.jsonl', required=True)
+    parser.add_argument('-o', '--output', help='Path to the output JSON. Default to current directory', required=False)
+    args = vars(parser.parse_args())
+
+    # label_key = get_node_label_attribute_key()
+
+    with get_database_client() as database_client, open(args["input"]) as data, open(args["output"] if args["output"] else "wiktextract-data.json", "w") as output:
+        output.write("[\n")
+
+        for line in data:
+            vocabulary = json.loads(line)
+            #logging.info(f"Loading vocabulary: {vocabulary}")
+            if "lang" in vocabulary and vocabulary["lang"] == "German":
+                term = vocabulary["word"]
+                # database_client.save_a_node_with_attributes("Term", {label_key: term, "language": GERMAN})
+
+                definitions = [sense["glosses"][0] if "glosses" in sense else sense["raw_glosses"][0] for sense in
+                               vocabulary["senses"]]
+                # for definition in definitions:
+                #     database_client.save_a_node_with_attributes("Definition", {label_key: definition})
+
+                #     database_client.save_a_link_with_attributes(
+                #         language=GERMAN,
+                #         source_label=term,
+                #         target_label=definition,
+                #         attributes={label_key: "definition"}
+                #     )
+
+                output.write(json.dumps({"term": term, "definitions": definitions}))
+                output.write(",\n")
+
+        output.seek(-2, os.SEEK_END)
+        output.truncate()
+        output.write("\n]\n")
